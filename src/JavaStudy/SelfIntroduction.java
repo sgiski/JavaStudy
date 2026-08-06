87a938feff8c2c1dac94c91150872357e71e2c2a
sgiski <speedsterland@gmail.com>
2026-08-06 20:38:37 +0900

    자기소개

---
+++ src/JavaStudy/SelfIntroduction.java
@@ -0,0 +1,9 @@
+package JavaStudy;
+
+public class SelfIntroduction {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요 반갑습니다");
+        System.out.println("저는 무직 백수 이영석 입니다");
+        System.out.println("잘 부탁 드립니다~");
+    }
+}
