fe38ec823b3c1a6001a7185fac5a309a77bee17e
sgiski <speedsterland@gmail.com>
2026-08-06 20:44:17 +0900

    프로필

---
+++ src/JavaStudy/Profile.java
@@ -0,0 +1,15 @@
+package JavaStudy;
+
+public class Profile {
+    public static void main(String[] args){
+        String name = "이영석";
+        int studentNumber = 20260729;
+        double tall =1.87;
+        boolean ismale = true;
+
+        System.out.println("이름: " + name);
+        System.out.println("학번: " + studentNumber);
+        System.out.println("신장: " + tall + "m");
+        System.out.println("남자인가요?" + ismale);
+    }
+}
