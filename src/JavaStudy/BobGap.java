204bffd86fd327f83370422f752f29a5b548d058
sgiski <speedsterland@gmail.com>
2026-08-07 18:09:59 +0900

    깃허브 테스트

--- src/JavaStudy/BobGap.java
+++ src/JavaStudy/BobGap.java
@@ -1,41 +1,49 @@
-package JavaStudy;
-//05:밥값 계산하기
-//돈까스 16인분, 칼국수 8인분, 왕만두 1인분을 시켰을때, 계산서를 출력 예와 같이 작성해 주세요. 가격표는 아래와 같습니다.
-//돈까스: 8000원
-//칼국수: 6000원
-//왕만두: 5000원
-
-//출력 예
-//돈가스 x 16 = 128000
-//칼국수 x  8 =  80000
-//왕만두 x  1 =   5000
-//===================
-//총합: 213000
-
-public class BobGap {
-    public static void main(String[] args){
-        int DonPrice = 8000; //돈가스 가격
-        int KalPrice = 10000; //칼국수
-        int KingPrice = 5000; //왕만두
-
-        int DonNumber = 16; //돈가스 수량
-        int KalNumber = 8; //칼국수
-        int KingNumber = 1; //왕만두
-
-        //결과 출력
-        System.out.printf("%s x %2d = %6d\n", "돈가스", DonNumber, DonPrice * DonNumber);
-        System.out.printf("%s x %2d = %6d\n", "칼국수", KalNumber, KalPrice * KalNumber);
-        System.out.printf("%s x %2d = %6d\n", "왕만두", KingNumber, KingPrice * KingPrice);
-        System.out.println("===================");
-        int sum = DonPrice * DonNumber + KalPrice * KalNumber + KingPrice * KingNumber;
-        System.out.printf("%s: %d\n", "총합", sum);
-
-
+//package JavaStudy;
+////05:밥값 계산하기
+////돈까스 16인분, 칼국수 8인분, 왕만두 1인분을 시켰을때, 계산서를 출력 예와 같이 작성해 주세요. 가격표는 아래와 같습니다.
+////돈까스: 8000원
+////칼국수: 10000원
+////왕만두: 5000원
+//
+////출력 예
+////돈가스 x 16 = 128000
+////칼국수 x  8 =  80000
+////왕만두 x  1 =   5000
+////===================
+////총합: 213000
+//
+//public class BobGap {
+//    public static void main(String[] args){
+////        int DonPrice = 8000; //돈가스 가격
+////        int KalPrice = 10000; //칼국수
+////        int KingPrice = 5000; //왕만두
+////
+////        int DonNumber = 16; //돈가스 수량
+////        int KalNumber = 8; //칼국수
+////        int KingNumber = 1; //왕만두
+////
+////        //결과 출력
+////        System.out.printf("%s x %2d = %6d\n", "돈가스", DonNumber, DonPrice * DonNumber);
+////        System.out.printf("%s x %2d = %6d\n", "칼국수", KalNumber, KalPrice * KalNumber);
+////        System.out.printf("%s x %2d = %6d\n", "왕만두", KingNumber, KingPrice * KingPrice);
+////        System.out.println("===================");
+////        int sum = DonPrice * DonNumber + KalPrice * KalNumber + KingPrice * KingNumber;
+////        System.out.printf("%s: %d\n", "총합", sum);
+//
+//
+//        //주저리주저리 이렇게 하면 어떨지 연습하는중
 //        String food1 = "돈가스";
 //        int price1 = 8000;
+//        int quantity1 =16;
 //        String food2 = "칼국수";
 //        int price2 = 10000;
+//        int quantity2 =8;
 //        String food3 = "왕만두";
 //        int price3 = 5000;
-    }
-}
+//        int quantity3 = 1;
+//
+//        System.out.printf("돈가스","X",quantity1,"=",price1 * quantity1 );
+//        System.out.printf();
+//        System.out.printf();
+//    }
+//}
--- src/JavaStudy/operator.java
+++ src/JavaStudy/operator.java
@@ -1,4 +1,38 @@
 package JavaStudy;
+//08:각 자리수의 합
+
+//문제
+//세자리의 정수 N (100~999)을 표현하는 각 자리의 숫자들의 합을 구하는
+//프로그램을 작성하세요.
+//[예] 932 정수의 각 자리 숫자들의 합계는 14 ( = 9 + 3 + 2)
+
+//입력 예 : 474
+//출력 예 : 15
+
+//힌트
+//%연산자와 /연산자를 활용하세요.
+//932 % 10 = 2
+//932 / 10 = 93
+
+import java.util.Scanner;
 
 public class operator {
+    public static void main(String[] args) {
+        int num = 0;
+        //int(integer, 정수), 정수 데이터를 저장하는 자료형
+        //num: 사용할 변수의 이름
+        // =0; : 변수에 처음넣는 값이 0
+        int sum = 0;
+
+        System.out.print("숫자를 입력하세요: ");
+        Scanner input = new Scanner(System.in);
+        num = input.nextInt();
+
+        while(num!=0){
+            sum += num%10;
+            num /= 10;
+        }
+        System.out.println("각 자리수의 합은 " +sum+"입니다.");
+        System.out.println("프로그램을 종료합니다.");
+    }
 }
