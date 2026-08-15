53851eeb54f22ca181781fe07620153fc029d8fa
sgiski <speedsterland@gmail.com>
2026-08-15 17:15:51 +0900

    구구단

--- src/JavaStudy/gugudan.java
+++ src/JavaStudy/gugudan.java
@@ -1,4 +1,65 @@
 package JavaStudy;
 
 public class gugudan {
+    public static void main(String[] args){
+        for (int a = 1; a<= 9;a++){
+            System.out.println(a);
+            for (int b = 1; b<=9; b++){
+                System.out.printf("%d * %d = %d\n", a, b, (a*b));
+            }
+        }
+    }
+
+
+////*    제어연산자
+////**
+////***
+////****
+////*****
+//
+////    public static void main(String[] args) {
+////        for (int a = 0; a <= 5; a++) {
+////            for (int b = 0; b < a; b++) {
+////                System.out.print("*");
+////            }
+////            System.out.println();
+////        }
+////    }
+//
+//
+//    /*
+//
+//     *
+//     **
+//     ***
+//     ****
+//     *****
+//
+//     */
+//
+////    public static void main(String[] args) {
+////
+////        int a;
+////        int b;
+////
+////        for (a = 0; a < 5; a++) {
+////            for (b = 0; b < 4-a; b++) {
+////                System.out.print("");
+////            }
+////            for (b = 0; b <= a; b++) {
+////                System.out.print("*");
+////            }
+////            System.out.println();
+////        }
+////    }
+//
+//    /*
+//
+//        *
+//       ***
+//      *****
+//     *******
+//    *********
+//
+//     */
 }
